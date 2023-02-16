5c2b8bc776fb387ac940232ba4d8a8d561fc07a8
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-02-16 22:18:13 +0200

    Add files via upload

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -14,8 +14,8 @@ yum install -y docker
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ec2-user
-aws ecr get-login-password --region=us-east-1 | docker login --username AWS --password-stdin ${var.aws_ecr_repository_url}
-docker run -d --name app -p 8080:80 ${var.aws_ecr_repository_url}:test
+aws ecr get-login-password --region=us-east-1 | docker login --username AWS --password-stdin "${var.aws_ecr_repository_url}"
+docker run -d --name app -p 8080:80 "${var.aws_ecr_repository_url}":test
   EOF
   
   iam_instance_profile   = var.instance_profile
